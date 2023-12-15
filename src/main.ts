1b1f3d13b1543b559810fffe6c7c64171a235cec
Lemming <l.interlaken.lem@gmail.com>
2023-12-15 21:01:03 +0800

    refactor:优化滚动条

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import "./styles.css";
+import "./styles/scrollbar.css";
+import "./styles/ant.css";
 import App from "./App.vue";
 import router from "./router";
 
