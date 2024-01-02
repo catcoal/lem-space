3e199b3b705959b34549522bdd308da8e677bfe4
Lemming <069solis@gmail.com>
2024-01-02 15:52:32 +0800

    fix(Virtualizer):修复虚拟列表缩放调整行数量时，最后一列数据错误问题

--- src/services/test.ts
+++ src/services/test.ts
@@ -1,5 +1,6 @@
 import LemFetch from "@/utils/MyFetch";
 
 export const GetRandomImages = () => {
+  // https://picsum.photos/v2/list
   return new LemFetch("/random_image").get("/list?page=2&limit=60");
 };
--- src/views/resouces/components/Resource/tanstack-virtual.vue
+++ src/views/resouces/components/Resource/tanstack-virtual.vue
@@ -1,10 +1,8 @@
 <!-- 文档 https://tanstack.com/virtual/v3/docs/api/virtualizer -->
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, nextTick } from 'vue'
 import { useVirtualizer } from '@tanstack/vue-virtual'
 import ResourceItem from "./resource-item.vue";
-// import { useResourceViewStore } from "@/stores/resourceView";
-// const ResourceViewStore = useResourceViewStore();
 
 const props = defineProps<{
     resources: Resource[];
@@ -12,6 +10,7 @@ const props = defineProps<{
 }>()
 
 
+// 初始配置
 const VirtualizerOptions = computed(() => {
     return {
         count: props.resources?.length, // 总数
@@ -23,14 +22,15 @@ const VirtualizerOptions = computed(() => {
 })
 
 const rowVirtualizer = useVirtualizer(VirtualizerOptions);
-
 const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems());
 const totalSize = computed(() => rowVirtualizer.value.getTotalSize());
+const itemGap = computed(() => 20); // 元素间距
 
 // 动态调整大小
-const measureElement = (el: any) => {
+const measureElement = async (el: any) => {
     if (!el) return;
-    rowVirtualizer.value.measureElement(el)
+    await nextTick(); // 防止数据调整错位
+    rowVirtualizer.value.measureElement(el);
     return undefined
 }
 </script>
@@ -41,14 +41,13 @@ const measureElement = (el: any) => {
         position: 'relative',
     }">
         <div v-for="(virtualRow, index) in virtualRows" :ref="measureElement" :key="virtualRow.key"
-            :data-size="virtualRows[index]?.size" :data-start="virtualRows[index].start"
-            :data-index="virtualRows[index].index" :style="{
+            :data-index="virtualRows[index].index" :data-lane="virtualRows[index].lane" :style="{
                 position: 'absolute',
                 top: 0,
                 left: 0,
                 width: '100%',
                 willChange: 'transform',
-                transform: `translate3d(0,${virtualRows[index].start}px,0)`,
+                transform: `translate3d(0,${virtualRows[index].start + (index * itemGap)}px,0)`,
             }">
             <ResourceItem :resource="resources[virtualRow.index]"></ResourceItem>
         </div>
